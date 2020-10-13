ceab10b6130c22d516098d57859d76a5d9eb7987
Lordva <louis.chabanon@gmail.com>
2020-10-13 09:56:41 +0200

    Updated the ip command to dynamicly get the version/number of players online from the server

--- commands/ip.js
+++ commands/ip.js
@@ -1,5 +1,13 @@
 const { ip_survival, version_survival, ip_creative } = require('../config.json')
 
 exports.run = async (client, message) => {
-    message.channel.send(`**IP :** ${ip_survival} \n **Version :** ${version_survival}`)
+    const utils = require('minecraft-server-util');
+
+    utils.ping("127.0.0.1", { port: 25567 }).then((responce) => {
+        const onlineplayers = responce.onlinePlayers
+        const serverVersion = responce.version
+        message.channel.send(`**IP :** ${ip_survival} \n 
+        **Version :** ${serverVersion} \n 
+        Il y a ${onlineplayers} joueurs en ligne`)
+    });
 }
