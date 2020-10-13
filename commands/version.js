6e3e3dba3939dcacf4c65e74180c1912d5da12f4
Lordva <louis.chabanon@gmail.com>
2020-10-13 09:26:45 +0200

    Updated the version command to dynamicly get the version from the server

--- commands/version.js
+++ commands/version.js
@@ -1,5 +1,13 @@
 const { version_survival } = require('../config.json');
 
+
 exports.run = async (client, message) => {
-    message.channel.send(`**Version :** ${version_survival}`)
+    const utils = require('minecraft-server-util');
+
+    utils.ping("127.0.0.1", { port: 25567}).then((response) => {
+        console.log(response);
+        const version = response.version;
+        message.channel.send(`**Version :** ${version}`)
+    })
+    
 }
