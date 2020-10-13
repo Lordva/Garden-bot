1f1291ff0f3d9738f6b956a5fd8162cf25d4af65
Lordva <louis.chabanon@gmail.com>
2020-10-13 09:58:28 +0200

    Fix syntax

--- commands/ip.js
+++ commands/ip.js
@@ -6,8 +6,6 @@ exports.run = async (client, message) => {
     utils.ping("127.0.0.1", { port: 25567 }).then((responce) => {
         const onlineplayers = responce.onlinePlayers
         const serverVersion = responce.version
-        message.channel.send(`**IP :** ${ip_survival} \n 
-        **Version :** ${serverVersion} \n 
-        Il y a ${onlineplayers} joueurs en ligne`)
+        message.channel.send(`**IP :** ${ip_survival} \n **Version :** ${serverVersion} \n Il y a ${onlineplayers} joueurs en ligne`)
     });
 }
