6445eb18f50e901d3acdf6a22bf565e1b7d4edee
Lordva <louis.chabanon@gmail.com>
2020-10-23 22:49:08 +0200

    added binary decoder

--- commands/binaire.js
+++ commands/binaire.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
     } else if (args[0] === 'encode') {
         const text = args.slice(1).join(' ');
         if(!text){
-            return message.channel.send(':x: Merci de rentrer un texte valide');
+            return message.channel.send(':x: Merci de rentrer un text valide');
         }else if(text.lenght >= 1024) {
             return message.channel.send(':x: votre message doit faire moins de 1044 characteres');
         };
@@ -33,6 +33,31 @@ exports.run = async (client, message, args) => {
             .setTimestamp();
         
         message.channel.send(embed);
+    } else if (args[0] === 'decode'){
+        const text = args.slice(1).join(' ');
+        if(!text){
+            return message.channel.send(':x: Merci de rentrer un text valide');
+        }else if(text.lenght >= 1024) {
+            return message.channel.send(':x: votre message doit faire moins de 1044 characteres');
+        };
+
+        const url = `http://some-random-api.ml/binary?decode=${text}`;
+        let response;
+        try{
+            response = await fetch(url).then(res => res.json());
+        }
+        catch (e){
+            return message.channel.send(":x: Une erreur c'est produite !" + e)
+        }
+
+        const embed = new RichEmbed()
+                .setColor('GREEN')
+                .setTitle('Décodeur Binaire')
+                .addField('Input', `\`\`\`\n${text}\`\`\``)
+                .addField('Output', `\`\`\`\n${response.text}\`\`\``)
+                .setFooter(`Demandé par ${message.author.tag}`)
+                .setTimestamp();
+                
+        message.channel.send(embed);
     }
-    
 }
